7845db7480c41bb1f2e55686f2222411bad6f5a7
PriyaGupta1996 <priyagupta.ynr@gmail.com>
2023-04-04 14:33:09 -0400

    implement fetch stats and show in boxes

--- src/App/InputForm.jsx
+++ src/App/InputForm.jsx
@@ -1,18 +1,24 @@
 import { useState } from "react";
 import axios from "axios";
 const filmsEndpointURL = "/api/assessments/films";
+import Films from "../Films";
 
 const InputForm = () => {
   const [name, setName] = useState("");
+  const [filmData, setFilmData] = useState([]);
+  const [show, setShow] = useState(false);
 
-  const OnSubmitHandler = async (name) => {
+  const OnSubmitHandler = async () => {
+    console.log(name);
     const filmData = await axios.get(filmsEndpointURL, {
       params: { directorName: name },
       headers: {
         Authorization: "Bearer 8c5996d5-fb89-46c9-8821-7063cfbc18b1",
       },
     });
-    console.dir(filmData, { depth: null });
+    if (name.length > 0) setShow(true);
+    else setShow(false);
+    setFilmData(filmData.data);
   };
 
   return (
@@ -28,11 +34,12 @@ const InputForm = () => {
         <button
           className="submit-button"
           id="submit-button"
-          onClick={() => OnSubmitHandler(name)}
+          onClick={OnSubmitHandler}
         >
           <span className="submit-button-text">Submit</span>
         </button>
       </div>
+      {show && <Films filmData={filmData} />}
     </>
   );
 };
--- src/Films/index.js
+++ src/Films/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './style.css';
 
 const axios = require('axios');
+const moment = require('moment');
 
 const filmsEndpointURL = "https://app.codescreen.com/api/assessments/films";
 
@@ -11,12 +12,44 @@ const apiToken = "8c5996d5-fb89-46c9-8821-7063cfbc18b1"
 
 export default class Films extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = { bestRated: "", averageFilm: "", shortestDays: "", longestFilm: "" };
+  }
+
+  componentDidMount() {
+    const filmData = this.props.filmData;
+    console.log("filmData", filmData)
+    if (filmData) {
+      const bestRated = this.getBestRatedFilm(filmData)
+      const longestFilm = this.getLongestFilm(filmData)
+      const averageFilm = this.getAverageRating(filmData)
+      const shortestDays = this.getShortestNumberOfDaysBetweenFilmReleases(filmData)
+      this.setState({ bestRated, longestFilm, averageFilm, shortestDays })
+    }
+  };
+
   /**
     * Retrieves the name of the best rated film from the given list of films.
     * If the given list of films is empty, this method should return "N/A".
   */
   getBestRatedFilm(films) {
-    //TODO Implement
+    if (films.length > 0) {
+      const result = films.reduce((accumulator, film) => {
+        if (accumulator["rating"] === undefined) {
+          accumulator["name"] = film["name"]
+          accumulator["rating"] = film["rating"]
+        }
+        else if (accumulator["rating"] < film["rating"]) {
+          accumulator["name"] = film["name"]
+          accumulator["rating"] = film["rating"]
+        }
+        return accumulator
+      }, {})
+      return result.name
+    }
+    return "NA"
+
   }
 
   /**
@@ -27,7 +60,19 @@ export default class Films extends Component {
     * For example, if the duration of the longest film is 120, this function should return "120 mins".
   */
   getLongestFilm(films) {
-    //TODO Implement
+    if (films.length > 0) {
+      const result = films.reduce((accumulator, film) => {
+        if (accumulator["length"] === undefined) {
+          accumulator["length"] = film["length"]
+        }
+        else if (accumulator["length"] < film["length"]) {
+          accumulator["length"] = film["length"]
+        }
+        return accumulator
+      }, {})
+      return result.length + " mins"
+    }
+    return "NA"
   }
 
   /**
@@ -35,7 +80,14 @@ export default class Films extends Component {
     * If the given list of films is empty, this method should return 0.
   */
   getAverageRating(films) {
-    //TODO Implement
+    if (films.length > 0) {
+      const result = films.reduce((accumulator, film) => {
+        return accumulator + film["rating"]
+      }, 0)
+      return (result / films.length).toFixed(1)
+    }
+    return 0
+
   }
 
   /**
@@ -73,7 +125,42 @@ export default class Films extends Component {
     * then this method should return 147, as Prestige was released 147 days after Batman Begins.
   */
   getShortestNumberOfDaysBetweenFilmReleases(films) {
-    //TODO Implement
+    if (films.length === 0)
+      return "NA"
+    if (films.length === 1)
+      return 0;
+
+    const filmsDate = films.map(film => (
+      new Date(film["releaseDate"])
+    ))
+    filmsDate.sort()
+    console.log(filmsDate)
+    const diffInMs = filmsDate[filmsDate.length - 1].getTime() - filmsDate[filmsDate.length - 2].getTime();
+    return Math.round(diffInMs / (1000 * 60 * 60 * 24));
   }
 
+
+
+  render() {
+    return (
+      <div className="stats-boxes">
+        <div id="best-rated-film" className='stats-box'>
+          <span className='stats-box-heading'>Best Rated Film</span>
+          <span className='stats-box-info'>{this.state.bestRated}</span>
+        </div>
+        <div id="longest-film" className='stats-box'>
+          <span className='stats-box-heading'>Longest Film Duration</span>
+          <span className='stats-box-info'>{this.state.longestFilm}</span>
+        </div>
+        <div id="average-rating" className='stats-box'>
+          <span className='stats-box-heading'>Average Rating</span>
+          <span className='stats-box-info'>{this.state.averageFilm}</span>
+        </div>
+        <div id="shortest-days" className='stats-box'>
+          <span className='stats-box-heading'>Shortest days between releases</span>
+          <span className='stats-box-info'>{this.state.shortestDays}</span>
+        </div>
+      </div >
+    );
+  }
 }
